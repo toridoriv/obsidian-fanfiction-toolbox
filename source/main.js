9309e17ce38b7dd20df962205536d9ee33918db3
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-03-29 11:45:37 -0300

    🔥 Remove unused code and dependencies
    This removes the unused CreateFandomFolder modal and related code, as well as the @wcj/markdown-to-html dependency and the remove-double-spaces command.

--- source/main.js
+++ source/main.js
@@ -1,45 +1,13 @@
-import markdown from "@wcj/markdown-to-html";
-import { App, Modal, Notice, Plugin } from "obsidian";
+import { App, Plugin } from "obsidian";
 
-import { getContentWithoutFrontMatter } from "./helpers/document.js";
 import tasks from "./tasks.js";
 
 const DEFAULT_SETTINGS = {};
-
-class CreateFandomFolder extends Modal {
-  /**
-   * @param {App} app
-   */
-  constructor(app) {
-    super(app);
-  }
-
-  onOpen() {
-    let { contentEl } = this;
-    contentEl.setText("Look at me, I'm a modal! 👀");
-  }
-
-  onClose() {
-    let { contentEl } = this;
-    contentEl.empty();
-  }
-}
-
 export default class FanfictionToolbox extends Plugin {
   settings = DEFAULT_SETTINGS;
 
   async onload() {
     await this.loadSettings();
-
-    this.addCommand({
-      id: "remove-double-spaces",
-      name: "Remove Double Spaces",
-      editorCallback: (editor) => {
-        const text = editor.getDoc().getValue();
-        const newText = text.replace(/\b\s{2}\b/g, " ");
-        editor.getDoc().setValue(newText);
-      },
-    });
 
     for (const task of tasks) {
       task.register(this);
