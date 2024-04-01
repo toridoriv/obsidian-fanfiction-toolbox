59b83195da0c8ee57bc5088af70f25ec49c9ea1d
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-04-01 20:17:47 -0300

    ✨ Add Expand utility type
    This provides a way to recursively or shallowly expand object types, while allowing certain types to be excluded from expansion.

--- source/global.d.ts
+++ source/global.d.ts
@@ -7,6 +7,70 @@ declare global {
 
   type FanfictionToolboxPlugin = FanfictionToolbox;
 
+  /**
+   * Takes a type `T` and expands it recursively or one level deep based on the `recursively` option.
+   *
+   * If `recursively` is `true`, uses {@linkcode Expand.Recursive}, else it uses {@linkcode Expand.OneLevel}.
+   *
+   * The type `E` is used to specify types that should not be expanded, but returned as they are. The default
+   * exclusions can be checked in {@linkcode Expand.ExpandExclusions}.
+   */
+  type Expand<
+    T,
+    recursively extends boolean = false,
+    E = Expand.Exclusions,
+  > = recursively extends true ? Expand.Recursive<T, E> : Expand.OneLevel<T, E>;
+
+  namespace Expand {
+    type Exclusions =
+      | ArrayBuffer
+      | Blob
+      | Date
+      | FormData
+      | Headers
+      | Map<any, any>
+      | Primitive
+      | ReadableStream<any>;
+
+    /**
+     * Takes a type `T` and expands it into an object type with the same properties as
+     * `T`.
+     *
+     * Replaces any properties and array elements in `T` with their expanded types, up to
+     * one level deep.
+     *
+     * `E` specifies types that should not be expanded but returned as-is.
+     */
+    type OneLevel<T, E = Exclusions> = T extends E
+      ? T
+      : T extends (...args: infer A) => infer R
+        ? (...args: OneLevel<A, E>) => OneLevel<R, E>
+        : T extends Promise<infer U>
+          ? Promise<OneLevel<U, E>>
+          : T extends object
+            ? { [K in keyof T]: T[K] }
+            : T;
+
+    /**
+     * Takes a type `T` and expands it into an object type with the same properties as
+     * `T`.
+     *
+     * Replaces any properties and array elements in `T` with their expanded types,
+     * recursively.
+     *
+     * `E` specifies types that should not be expanded but returned as-is.
+     */
+    type Recursive<T, E = Exclusions> = T extends E
+      ? T
+      : T extends (...args: infer A) => infer R
+        ? (...args: Recursive<A, E>) => Recursive<R, E>
+        : T extends Promise<infer U>
+          ? Promise<Recursive<U, E>>
+          : T extends object
+            ? { [K in keyof T]: Recursive<T[K], E> }
+            : T;
+  }
+
   namespace Obsidian {
     type Command = obsidian.Command;
     type CommandEditorCallback = (
