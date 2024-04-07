8b4ab4eaf66f4f24c4c248f973d423f016afb3e4
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-04-07 13:13:10 -0400

    ♻️ Refactor plugin structure
    This improves the organization and modularity of the plugin code.

--- source/main.js
+++ source/main.js
@@ -1,24 +1,3 @@
-import { App, Plugin } from "obsidian";
+import { FanfictionToolbox } from "./plugin.js";
 
-import tasks from "./tasks.js";
-
-const DEFAULT_SETTINGS = {};
-export default class FanfictionToolbox extends Plugin {
-  settings = DEFAULT_SETTINGS;
-
-  async onload() {
-    await this.loadSettings();
-
-    for (const task of tasks) {
-      task.register(this);
-    }
-  }
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
-}
+export default FanfictionToolbox;
---
+++ source/obsidian.js
@@ -0,0 +1,1 @@
+export * as obsidian from "obsidian";
