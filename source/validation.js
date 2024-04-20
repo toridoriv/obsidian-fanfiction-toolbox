af1cc7fa6c0cac89660062f2eb0ed4216790fc45
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-04-19 20:58:00 -0400

    🦺 Add utility functions for type checking and validation
    This provides reusable functions for checking types, validating values, and working with Obsidian's file system.

--- source/validation.js
+++ source/validation.js
@@ -1,3 +1,24 @@
+import { curry } from "./utilities.js";
+
+/**
+ * Represents an abstract file in the Obsidian vault. It can be either a regular file or a
+ * folder.
+ *
+ * @typedef {import("obsidian").TAbstractFile} TAbstractFile
+ */
+
+/**
+ * Represents a folder in the Obsidian vault.
+ *
+ * @typedef {import("obsidian").TFolder} TFolder
+ */
+
+/**
+ * Represents a file in the Obsidian vault.
+ *
+ * @typedef {import("obsidian").TFile} TFile
+ */
+
 /**
  * Coerces a value to a specified type.
  *
@@ -8,6 +29,43 @@
 function coerce(value) {
   // eslint-disable-next-line prettier/prettier
   return /** @type {T} */ (value);
+}
+
+/**
+ * Returns the input value unchanged.
+ *
+ * This function is useful in scenarios where you need to:
+ * - Pass a value through a sequence of functions without modifying it.
+ * - Satisfy a function or method that expects a callback, but you don't need to perform
+ * any operations.
+ * - Serve as a default or fallback function when no specific transformation is required.
+ *
+ * @template T - The type of the input value.
+ * @param {T} value - The value to be returned.
+ * @returns {T} The original input value.
+ */
+function identity(value) {
+  return value;
+}
+
+/**
+ * Creates a tuple from the provided values.
+ *
+ * A tuple is an array with a fixed number of elements, where each element can have a
+ * different type.
+ * This function takes any number of arguments and returns them as a tuple.
+ *
+ * @template {any[]} T - The types of the tuple elements.
+ * @param {T} values - The values to include in the tuple.
+ * @returns {T} A tuple containing the provided values.
+ * @example
+ *
+ * const myTuple = tuple(1, 'hello', true);
+ * console.log(myTuple); // Output: [1, 'hello', true]
+ *
+ */
+function tuple(...values) {
+  return values;
 }
 
 /**
@@ -22,6 +80,26 @@ function coerce(value) {
 function defined(value, message = "Expected a defined value, but received `undefined`.") {
   if (value === undefined) {
     throw new TypeError(message);
+  }
+
+  return coerce(value);
+}
+
+/**
+ * Ensures the given value is non-nullable (not `null` or `undefined`).
+ *
+ * @template T - The type of the value.
+ * @param {T}      value     - The value to check.
+ * @param {string} [message] - A custom error message.
+ * @returns {Exclude<T, undefined | null>} The original value if defined.
+ * @throws {TypeError} If the value is undefined or null.
+ */
+function nonNullable(
+  value,
+  message = "Expected a non-nullable value, but received TYPEOF.",
+) {
+  if (value === undefined || value === null) {
+    throw new TypeError(message.replace("TYPEOF", typeof value));
   }
 
   return coerce(value);
@@ -102,4 +180,88 @@ function hasProperty(value, key) {
   );
 }
 
-export { areEqualType, coerce, defined, hasProperty, isDefined, isObject };
+/**
+ * Checks if a given entry is a file.
+ *
+ * @param {TAbstractFile} entry - The entry to check.
+ * @returns {entry is TFile} Returns true if the entry is a file (has a string extension
+ *                           property), false otherwise.
+ */
+function isFile(entry) {
+  // @ts-ignore: ¯\_(ツ)_/¯
+  return typeof entry.extension === "string";
+}
+
+/**
+ * Checks if a given entry is a folder.
+ *
+ * @param {TAbstractFile} entry - The entry to check.
+ * @returns {entry is TFolder} Returns true if the entry is a folder (does not have an
+ *                             extension property),
+ *                             false otherwise.
+ */
+function isFolder(entry) {
+  // @ts-ignore: ¯\_(ツ)_/¯
+  return typeof entry.extension === "undefined";
+}
+
+/**
+ * Checks if an entry has a specific name.
+ *
+ * @param {string}        name  - The name to compare against the entry's name.
+ * @param {TAbstractFile} entry - The entry to check.
+ * @returns {boolean} Returns true if the entry's name matches the given name, false
+ *                    otherwise.
+ */
+function isEntryWithName(name, entry) {
+  return entry.name === name;
+}
+
+/**
+ * Checks if a given entry is the "Metadata.md" file.
+ */
+const isMetadataFile = curry(isEntryWithName)("Metadata.md");
+
+/**
+ * Checks if a given entry is the "Worldbuilding.md" file.
+ */
+const isWorldbuildingFile = curry(isEntryWithName)("Worldbuilding.md");
+
+/**
+ * Checks if a string matches a given pattern.
+ *
+ * @param {string | RegExp} pattern - The pattern to match against. Can be a string or a
+ *                                  regular expression.
+ * @param {string}          value   - The string to check for a match.
+ * @returns {boolean} Returns true if the string matches the pattern, false otherwise.
+ * @example
+ *
+ * const pattern = /hello/i;
+ * const value = "Hello, world!";
+ * const isMatch = matchesPattern(pattern, value);
+ * console.log(isMatch); // Output: true
+ *
+ */
+function matchesPattern(pattern, value) {
+  const regex = new RegExp(pattern);
+
+  return regex.test(value);
+}
+
+export {
+  areEqualType,
+  coerce,
+  defined,
+  hasProperty,
+  identity,
+  isDefined,
+  isEntryWithName,
+  isFile,
+  isFolder,
+  isMetadataFile,
+  isObject,
+  isWorldbuildingFile,
+  matchesPattern,
+  nonNullable,
+  tuple,
+};
