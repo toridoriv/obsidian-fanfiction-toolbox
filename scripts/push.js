7949c533f7c6f4bcf4c3b9d934e3fdec0b5405ef
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-04-26 19:32:00 -0400

    🔧 Update push script to target different vaults and copy additional files
    - The `push.js` script now supports both development and non-development
    environments by targeting different vaults.
    - The target directory is created automatically if it doesn't exist.
    - In addition to `main.js` and `manifest.json`, the script
    also copies `styles.css`.

--- scripts/push.js
+++ scripts/push.js
@@ -3,12 +3,17 @@ import fs from "node:fs";
 
 import { manifest } from "../source/manifest.js";
 
-const vault = process.env.FANFICTION_DIR;
+const vault =
+  process.env.NODE_ENV === "development"
+    ? "./example"
+    : process.env.FANFICTION_TOOLBOX_TEMPLATE_VAULT;
 const target = `${vault}/.obsidian/plugins/${manifest.id}`;
+const originDir = `${process.env.DEVELOPMENT_DIR_PERSONAL}/Obsidian-Fanfiction-Toolbox`;
 
 if (!fs.existsSync(target)) {
-  fs.mkdirSync(target);
+  fs.mkdirSync(target, { recursive: true });
 }
 
-fs.copyFileSync("./dist/main.js", `${target}/main.js`);
-fs.copyFileSync("./dist/manifest.json", `${target}/manifest.json`);
+fs.copyFileSync(`${originDir}/dist/main.js`, `${target}/main.js`);
+fs.copyFileSync(`${originDir}/dist/manifest.json`, `${target}/manifest.json`);
+fs.copyFileSync(`${originDir}/static/styles.css`, `${target}/styles.css`);
